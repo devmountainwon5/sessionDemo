bee3ce5ba08be58f2312570e6d7f3245fd8cd5d7
Joshua Smith <veryjoshua@gmail.com>
2019-05-29 21:58:36 -0400

    adding login/register

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const massive = require('massive');
 const session = require('express-session');
-const bcrypt = require('bycrpt');
+const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 require('dotenv').config();
@@ -26,7 +26,7 @@ app.use(session({
 
 app.use(bodyParser.json());
 
-app.post('/api/register', (req, res) => {
+app.post('/api/register', (req, res, next) => {
     // Get DB instance
     const db = req.app.get('db');
 
@@ -39,29 +39,64 @@ app.post('/api/register', (req, res) => {
         .then((user)=>{
             // Send back message if email was there
             if(user){
-                res.send('Sorry this email already exists. Please login.')
+               throw('Sorry this email already exists. Please login.')
             }else{ // Make New user
                 /// Encrypt password
-                bcrypt.hash(password, saltRounds, (err, hash) => {
-                    if(err){ // Handle error
-                        res.send('Something broke')
-                    }else{
-                        /// create new user with hashed password.
-                        return db.user_table.insert({email, password:hash})
-                    }
-                  });
+                return bcrypt.hash(password, saltRounds);
             }
         })
+        .then((hash) => {
+            /// create new user with hashed password.
+            return db.user_table.insert({email, password:hash})
+          })
         // assign user to session. 
         .then((user)=>{
-            console.log(user);
-            res.send('check')
+            //This is very very important
+            delete user.password;
+            //Assign user to session
+            req.session.user = user;
+            // Send message
+            res.send('Registered!')
+        })
+        .catch((err)=>{
+            res.send(err)
         })
 
 })
 
 app.post('/api/login', (req, res) => {
-    
+    // Get Db instance
+    const db = req.app.get('db');
+
+    // Set up vars from req.body
+    const {email, password} = req.body;
+    let currentUser;
+    // Try to login. 
+    //1. Check to make sure is user
+    db.user_table.findOne({email})
+        .then((user)=>{
+            if(!user){
+                throw('No user found. Please Register')
+            }else{
+    //2. Check password
+                currentUser = user; 
+                return bcrypt.compare(password, user.password)            
+            }
+        })
+    //3. If its a match assign user to session else throw error?    
+        .then((correctPassword) => {
+            if(correctPassword){
+                delete currentUser.password;
+                req.session.user =currentUser;
+                res.send('Logged in')
+            }else{
+                throw('Sorry your credentials didnt match')
+            }
+        })
+        .catch((err)=>{
+            res.send(err)
+        })
+
 })
 
 app.use((req, res)=>{
